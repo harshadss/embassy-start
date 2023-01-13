a1d93ff829f5599b5488ad2608be7db2d629ff90
Harshad Saykhedkar <harshad@Harshads-MacBook-Air.local>
2023-01-13 12:49:09 +0530

    Simple timers

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,34 @@
 #![feature(type_alias_impl_trait)]
 #![no_main]
 
-use defmt::info;
+use defmt::{info, unwrap};
 use  {defmt_rtt as _, panic_probe as _};
 
 use embassy_executor::Spawner;
-use embassy_nrf::interrupt;
-use embassy_nrf::timer::Timer;
+// use embassy_nrf::interrupt;
+use embassy_time;
 
+#[embassy_executor::task]
+async fn run1() {
+    loop {
+        embassy_time::Timer::after(embassy_time::Duration::from_millis(500)).await;
+        info!("Tick from fast timer");
+    }
+}
+
+#[embassy_executor::task]
+async fn run2() {
+    loop {
+        embassy_time::Timer::after(embassy_time::Duration::from_millis(2000)).await;
+        info!("Tick from slow timer");
+    }
+}
+
+/*
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_nrf::init(Default::default());
-    let mut t = Timer::new_awaitable(p.TIMER0, interrupt::take!(TIMER0));
+    // let mut t = Timer::new_awaitable(p.TIMER0, interrupt::take!(TIMER0));
 
     // all timers are implemented as registers in uc
     // for 1 MHz default frequency, 1_000_000 count will strike after 1 second
@@ -25,4 +42,13 @@ async fn main(_spawner: Spawner) {
         t.cc(0).wait().await;
         info!("hardware timer tick");
     }
+}
+*/
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let _p = embassy_nrf::init(Default::default());
+
+    unwrap!(spawner.spawn(run1()));
+    unwrap!(spawner.spawn(run2()));
 }
