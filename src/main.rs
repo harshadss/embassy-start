0dc2a749cf96076e45e46c44c64aba6297815853
Harshad Saykhedkar <harshad@Harshads-MacBook-Air.local>
2023-01-23 15:42:12 +0530

    add accelerometer code

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,21 @@
 #![feature(type_alias_impl_trait)]
 #![no_main]
 
+use accelerometer::Accelerometer;
 use defmt::{info, unwrap};
 use {defmt_rtt as _, panic_probe as _};
 
 use embassy_executor::Spawner;
 use embassy_nrf::interrupt;
-use embassy_nrf::gpio::{Input, Pull};
+//use embassy_nrf::gpio::{Input, Pull};
 use embassy_nrf::temp::Temp;
 use embassy_time;
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy_sync::channel::Channel;
+
+pub mod  accelerometer;
+
+static SHARED: Channel<ThreadModeRawMutex, u16, 2> = Channel::new();
 
 #[embassy_executor::task]
 async fn run1() {
@@ -27,25 +34,23 @@ async fn run2() {
     }
 }
 
-/*
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    let p = embassy_nrf::init(Default::default());
-    // let mut t = Timer::new_awaitable(p.TIMER0, interrupt::take!(TIMER0));
-
-    // all timers are implemented as registers in uc
-    // for 1 MHz default frequency, 1_000_000 count will strike after 1 second
-    t.cc(0).write(1_000_000);
-    //
-    t.cc(0).short_compare_clear();
-    t.start();
-
+#[embassy_executor::task]
+async fn measure_vals(mut temp: Temp<'static>, mut acc: Accelerometer<'static>) {
     loop {
-        t.cc(0).wait().await;
-        info!("hardware timer tick");
+        let value = temp.read().await;
+        SHARED.send(value.to_num::<u16>()).await;
+        let acc_x = acc.accel_data();
+        match acc_x {
+            Ok(measurement) => {
+                info!("Accelerometer: z: {}", measurement.z);
+            },
+            Err(_) => {
+                info!("Error in reading acclerometer")
+            }
+        }
+        embassy_time::Timer::after(embassy_time::Duration::from_secs(1)).await;
     }
 }
-*/
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
@@ -53,19 +58,23 @@ async fn main(spawner: Spawner) {
 
     let temp_irq = interrupt::take!(TEMP);
 
-    let mut temp = Temp::new(p.TEMP, temp_irq);
-
-    let value = temp.read().await;
-
-    info!("Initial temperature: {}", value.to_num::<u16>());
+    let temp = Temp::new(p.TEMP, temp_irq);
 
     unwrap!(spawner.spawn(run1()));
     unwrap!(spawner.spawn(run2()));
 
-    let mut btn_a = Input::new(p.P0_14, Pull::Up);
 
+
+    // temp.read().await;
+
+    let receiver = SHARED.receiver();
+    let acc_irq = interrupt::take!(SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0);
+    let acc = accelerometer::Accelerometer::new(p.TWISPI0, acc_irq, p.P0_16, p.P0_08).unwrap();
+
+    unwrap!(spawner.spawn(measure_vals(temp, acc)));
     loop {
-        btn_a.wait_for_falling_edge().await;
-        info!("button a pressed");
+        let temp_val = receiver.recv().await;
+        info!("Temperature is: {}", temp_val);
     }
+
 }
